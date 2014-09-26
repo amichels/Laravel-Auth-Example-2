fabe0f056b7433f525fe0e4a06744b369a18f828
amichels <andy.j.michels@gmail.com>
2014-09-25 21:19:10 -0500

    login and dashboard

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -4,6 +4,7 @@ class UsersController extends BaseController {
 
 	public function __construct() {
 	    $this->beforeFilter('csrf', array('on'=>'post'));
+	    $this->beforeFilter('auth', array('only'=>array('getDashboard')));
 	}
 
     protected $layout = "layouts.main";
@@ -31,6 +32,20 @@ class UsersController extends BaseController {
 
 	public function getLogin() {
 	    $this->layout->content = View::make('users.login');
+	}
+
+	public function postSignin() {
+    	if (Auth::attempt(array('email'=>Input::get('email'), 'password'=>Input::get('password')))) {
+		    return Redirect::to('users/dashboard')->with('message', 'You are now logged in!');
+		} else {
+		    return Redirect::to('users/login')
+		        ->with('message', 'Your username/password combination was incorrect')
+		        ->withInput();
+		}      
+	}
+
+	public function getDashboard() {
+    	$this->layout->content = View::make('users.dashboard');
 	}
 }
 ?>
--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -12,17 +12,21 @@
     {{ HTML::script('js/modernizr.js') }}
 </head>
 <body>
-	<div class="navbar navbar-fixed-top">
-        <div class="navbar-inner">
-            <div class="container">
-                <ul class="nav">  
-                    <li>{{ HTML::link('users/register', 'Register') }}</li>   
-                    <li>{{ HTML::link('users/login', 'Login') }}</li>   
-                </ul>  
-            </div>
-        </div>
-    </div> 
-             
+    <nav class="top-bar" data-topbar role="navigation">
+      <ul class="title-area">
+        <li class="name">
+          <h1><a href="#">Cellar</a></h1>
+        </li>
+        <li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
+      </ul>
+
+      <section class="top-bar-section">
+        <ul class="right">
+            <li>{{ HTML::link('users/register', 'Register') }}</li>
+            <li>{{ HTML::link('users/login', 'Login') }}</li>
+        </ul>
+      </section>
+    </nav>   
  
     <div class="container">
         @if(Session::has('message'))
